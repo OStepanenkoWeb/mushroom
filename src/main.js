7d173736c15b4a5b17824d73d44b6c176630e202
stepoooleg <stepooleg@gmail.com>
2019-10-13 14:15:43 +0300

    create table and include vuetiy

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
 new Vue({
+  render: h => h(App),
+  vuetify,
   router,
-  store,
-  render: h => h(App)
+  store
 }).$mount('#app')
